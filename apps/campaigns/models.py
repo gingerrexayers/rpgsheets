0b8d7f8d24faef7c11b3fbfd99641d36a1a30e02
Sue Deng <spideng@gmail.com>
2016-09-28 13:28:04 -0700

    added campaign, campaigndescr, campaignmanage

--- apps/campaigns/models.py
+++ apps/campaigns/models.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
-
+from ..loginreg.models import User
 from django.db import models
 
 # Create your models here.
-class Campaign(models.model):
+class Campaign(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
     game_master = models.ForeignKey(User, related_name='campaigns')
--- apps/campaigns/views.py
+++ apps/campaigns/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'campaigns/lobby.html')
+
+def info(request, id):
+	return render(request, 'campaigns/campaignDesc.html')
+
+def manage(request, id):
+	return render(request, 'campaigns/campaignManage.html')
