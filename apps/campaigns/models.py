53d2ed658e07e2da0a6248878b57c98d1ec9bc22
Mark Ayers <markthema3@gmail.com>
2016-09-28 14:27:09 -0700

    add campaign manager

--- apps/campaigns/models.py
+++ apps/campaigns/models.py
@@ -3,6 +3,27 @@ from ..loginreg.models import User
 from django.db import models
 
 # Create your models here.
+class CampaignManager(models.Manager):
+    def add_player(self, campaign_id, form_data):
+        u = User.manager.get(id=form_data['user'])
+        c = self.get(id=campaign_id)
+        c.players.add(u)
+        return True
+    def update_info(self, campaign_id, form_data):
+        errors = []
+        if not form_data['name']:
+            errors.append('Name cannot be empty!')
+        if not form_data['description']:
+            errors.append('Description cannot be empty!')
+        if errors:
+            return (False, errors)
+
+        c = self.get(id=campaign_id)
+        c.name = form_data['name']
+        c.description = form_data['description']
+        c.save()
+        return (True, ['Campaign updated successfully!'])
+
 class Campaign(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255)
